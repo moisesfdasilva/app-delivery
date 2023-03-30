797b27a8ae5572ffc58cf098c1ce61044e026a52
moisesfdasilva <msesfernandes@gmail.com>
2023-03-29 21:16:33 -0300

    feat: criados os campos input de login, em front-end/src/pages/login/index.jsx[REQ.2].

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    disabled: true,
+  });
+
+  const handleValidation = () => {
+    const regex = /\S+@\S+\.\S+/;
+    const minLength = 5;
+    const verifyEmail = form.email && regex.test(form.email);
+    if (verifyEmail && form.password.length > minLength) {
+      setForm((prevState) => ({ ...prevState, disabled: false }));
+    } else {
+      setForm((prevState) => ({ ...prevState, disabled: true }));
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  useEffect(() => handleValidation(), [form.email, form.password]);
+
+  // const handleSubmit = () => {
+  //   const objectEmail = {
+  //     email: form.email,
+  //   };
+  //   localStorage.setItem('user', JSON.stringify(objectEmail));
+  //   history.push('/meals');
+  // };
+
   return (
-    <div>Tela de login</div>
+    <div>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            value={ form.email }
+            type="email"
+            name="email"
+            placeholder="email"
+            data-testid="common_login__input-email"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            value={ form.password }
+            type="password"
+            name="password"
+            placeholder="password"
+            data-testid="common_login__input-password"
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="common_login__button-login"
+          // onClick={ handleSubmit }
+          disabled={ form.disabled }
+        >
+          LOGIN
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
+    </div>
   );
 }
 
