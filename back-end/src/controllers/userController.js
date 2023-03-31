25a13ddb73b78a7d7723d5b3c13d508ce5672c55
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-03-31 09:34:07 -0300

    fix lint

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,14 +1,15 @@
+const md5 = require('md5');
 const userService = require('../service/user.service');
 const { mapError } = require('../utils/error/mapError');
-const md5 = require('md5');
 
 const postUserLogin = async (req, res) => {
   const { email, password } = req.body;
   const hash = md5(password);
   const user = await userService.postLogin(email, hash);
 
-  if(user.type) return res.status(mapError(user.type))
-    .json({ message: user.message });
+  if (user.type) {
+    return res.status(mapError(user.type)).json({ message: user.message });
+  }
 
   return res.status(200).json({ token: user });
 };
