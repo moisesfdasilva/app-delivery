b843f78c1323caaca55826fbfd08aa95f1cdd133
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-03-31 14:08:26 -0300

    feat(.)

--- back-end/src/service/user.service.js
+++ back-end/src/service/user.service.js
@@ -4,14 +4,14 @@ const jwtEncode = require('../utils/jwtToken/jwtEncode');
 const postLogin = async (email, password) => {
   const user = await Users.findOne({
     where: { email, password },
-    attributes: { exclude: ['password'] },
+    attributes: { exclude: ['password', 'id'] },
   });
 
   if (!user) return { type: 'USER_NOT_FOUND', message: 'Not found' };
 
   const token = jwtEncode({ user });
 
-  return token;
+  return { token, ...user };
 };
 
 module.exports = { postLogin };
