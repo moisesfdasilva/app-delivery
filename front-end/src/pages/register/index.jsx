bf7ca954a11835e81673f6bc052d7b99deecdb0e
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2023-03-31 17:27:31 -0300

    feat: implementa requisito 08

--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,48 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import UserContext from '../../store/context/UserContext';
 
 function Register() {
-  console.log('desenvolver tela de registro');
+  // const history = useHistory();
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    disabled: true,
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleValidation = () => {
+    const regex = /\S+@\S+\.\S+/;
+    const minLength = 6;
+    const nameMinLength = 12;
+    const verifyName = form.name.trim().length > nameMinLength;
+    const verifyEmail = form.email && regex.test(form.email);
+    if (verifyEmail && form.password.length >= minLength && verifyName) {
+      setForm((prevState) => ({ ...prevState, disabled: false }));
+    } else {
+      setForm((prevState) => ({ ...prevState, disabled: true }));
+    }
+  };
+
+  useEffect(() => handleValidation(), [form.name, form.email, form.password]);
+
   return (
     <div>
       <h2>CADASTRO</h2>
       <form>
         <label htmlFor="name">
-          Password:
+          Name:
           <input
-            // value={ form.name }
+            value={ form.name }
             type="name"
             name="name"
             placeholder="name"
             data-testid="common_register__input-name"
-            // onChange={ handleChange }
+            onChange={ handleChange }
           />
         </label>
         <label htmlFor="email">
           Email:
           <input
-            // value={ form.email }
+            value={ form.email }
             type="email"
             name="email"
             placeholder="email"
             data-testid="common_register__input-email"
-            // onChange={ handleChange }
+            onChange={ handleChange }
           />
         </label>
         <label htmlFor="password">
           Password:
           <input
-            // value={ form.password }
+            value={ form.password }
             type="password"
             name="password"
             placeholder="password"
             data-testid="common_register__input-password"
-            // onChange={ handleChange }
+            onChange={ handleChange }
           />
         </label>
         <button
           type="button"
           data-testid="common_register__button-register"
-        //   disabled={ form.disabled }
+          disabled={ form.disabled }
         >
           CADASTRAR
         </button>
