f238a561f91b68cfe7f444bdf0078c43a2ebf35c
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-04-03 10:26:46 -0300

    .

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -35,6 +35,8 @@ function Login() {
     switch (role) {
     case 'customer':
       return history.push('/customer/products');
+    case 'admin':
+      return history.push('/customer/products');
     default:
       return history.push('/');
     }
