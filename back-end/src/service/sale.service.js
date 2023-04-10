1bb4c6f0e9537141b56de23baed9396f0b299cd5
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-04-10 16:49:21 -0300

    fix: 20, 21

--- back-end/src/service/sale.service.js
+++ back-end/src/service/sale.service.js
@@ -1,17 +1,17 @@
 const { Sales, SalesProducts } = require('../database/models');
 
 const postSale = async (body) => {
-  const { sale, products } = body;
+  const { newSale, products } = body;
   console.log(products);
-  const newSale = await Sales.create({ ...sale, saleDate: Date.now() });
+  const salelml = await Sales.create({ ...newSale, saleDate: Date.now() });
 
-  await products.forEach((product) => SalesProducts.create({
-    saleId: newSale.id,
+  await Promise.all(products.map((product) => SalesProducts.create({
+    saleId: salelml.id,
     productId: product.id,
     quantity: product.quantity,
-  }));
+  })));
 
-  return newSale;
+  return salelml;
 };
 
 module.exports = {
